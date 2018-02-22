0ca6bb539bf2ed54eb7125bb6d24112304bc2435
hoge <@>
2018-02-22 11:26:02 +0900

    kadaisann

---
+++ Try3.java
@@ -0,0 +1,24 @@
+//ドッグクラスを作りたい。
+class Dog
+{
+	public Dog
+	{
+		String num = "";
+		String age = "";
+	}
+	public Dog(char n,char g)
+	{
+		char name = n;
+		char age = g;
+		
+		System.out.println("犬の名前は" + name + "としは" + age );
+	}
+}
+class Test3
+{
+	public static void main
+	{
+		Dog dog1 = new Dog("シロ",3);
+		Dog dog2 = new Dog("ホワイト")
+	}
+}
